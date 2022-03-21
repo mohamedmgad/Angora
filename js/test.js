9a66fe28225ef26c675323b0a56218ef07571036
mohamedmgad <gad6400@gmail.com>
2022-03-21 11:25:10 +0300

    Angora - Template

---
+++ js/test.js
@@ -0,0 +1,12 @@
+var x=window.prompt("enter");
+if(x== "back"){
+    window.alert("back");
+}else if(x=="front"){
+    window.alert("front");
+}else if(x=="ui"){
+    window.alert("ui");
+}else if(x=="asd"){
+    window.alert("asd");
+}else {
+    window.alert("asdasd");
+}
